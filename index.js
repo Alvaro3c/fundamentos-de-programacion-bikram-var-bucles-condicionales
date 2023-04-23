479f9c9dad4f0eb09143e10c4e1561fc2591a7e1
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-04-23 18:03:50 +0200

    Casi terminado, faltan algunos puntos sobre bucles

---
+++ index.js
@@ -0,0 +1,70 @@
+let variableSinValor = '';
+let booleano1 = true;
+let booleano2 = false;
+const pi = 3.14;
+const tau = 2 * pi;
+
+let booleanoAnd = booleano1 === booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) === (booleano1 || (!booleano1 === !booleano2))
+
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+let incrementarAntes = 2;
+let resultadoAntes = incrementarAntes--;
+
+let contarHata10_2 = 0;
+for (let contarHata10_2 = 0; contarHata10_2 < 11; contarHata10_2++) {
+    console.log(contarHata10_2)
+};
+
+/* 
+Este bucle bloquea la pagina
+
+postl = 0;
+postj = 0;
+
+for (let i = 0; i < 11; i++) {
+    i = postj
+    console.log(postj)
+}; */
+
+/* 
+sin acabar
+
+let sumarPares = 0;
+for (let i = 0; i < 10; i++) {
+    if (i % 2) {
+
+    }
+}; */
+
+let variableValorNumerico = 5;
+const MiNombre = 'Alvaro';
+const MiNumeroFav = 7
+
+let booleanoOr = booleano1 || booleano2;
+let booleanoMix1 = booleano1 && tau / 2 == pi;
+let seisNoEsNueve = 6 === 9;
+let booleanoMix2 = variableValorNumerico > 0 || -(MiNumeroFavorito * tau);
+
+let valorSuma = MiNumeroFav + variableValorNumerico;
+let valorResta = MiNumeroFav - variableValorNumerico;
+let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+let valoDivision = MiNumeroFav / variableValorNumerico;
+
+let contarHasta10 = 0;
+while (contarHasta10 < 10) {
+    contarHasta10++
+    console.log(contarHasta10)
+};
+
+let prel = 0;
+let prej = 0;
+
+let sumalmpares = 0;
+for (let i = 0; i < 10; i++) {
+    if (i % 2 === 1) {
+        sumalmpares + i
+    }
+}
